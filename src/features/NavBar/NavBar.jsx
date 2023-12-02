4b743b4704adf8798d34c59390a002f0fb75154a
Iulia Anca <iulia.anca@evozon.com>
2023-12-02 18:26:13 +0200

    Refactor NavBar

--- src/features/NavBar/NavBar.jsx
+++ src/features/NavBar/NavBar.jsx
@@ -1,12 +1,28 @@
 import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
+import clsx from "clsx";
 
 export const NavBar = () => {
     return (
         <nav className={styles.navBarContainer}>
-            <NavLink to="profilePageBrok">Profile Page</NavLink>
-            <NavLink to="login" className={styles.authButton}>Login</NavLink>
-            <NavLink to="register">Register</NavLink>
+            <NavLink 
+                to="profilePageBrok" 
+                className={({ isActive }) => isActive && styles.active}
+            >
+                Profile Page
+            </NavLink>
+            <NavLink 
+                to="login" 
+                className={({ isActive }) => clsx(styles.authButton, {[styles.active]: isActive})}
+            >
+                Login
+            </NavLink>
+            <NavLink 
+                to="register" 
+                className={({ isActive }) => isActive && styles.active}
+            >
+                Register
+            </NavLink>
         </nav>
     );
 }
